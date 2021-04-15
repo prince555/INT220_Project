b105b3266275b1656ab53e3f0626189a6f23cb5b
Prince Pinnu <princepinnu55@gmail.com>
2021-04-15 14:47:18 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Car Reservation</title>
+
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/jquery.dataTables.css">
+
+	</head>
+
+	<style type="text/css">
+		.navbar { margin-bottom:0px !important; }
+		.carousel-caption { margin-top:0px !important }
+		body, html{overflow-x: hidden;}
+	</style>
+
+
+
+	<body>
+
+			<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+				<div class="container-fluid">
+				
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<img src="img/carlogo.jpg" height="50" width="50"> &nbsp;
+					</div>
+			
+					<div class="collapse navbar-collapse navbar-ex1-collapse">
+						<ul class="nav navbar-nav">
+							<li><a href="#" style="font-family: Times New Roman; font-size: 30px;">Car Reservation</a></li>
+						</ul>
+
+						<ul class="nav navbar-nav" style="font-family: Times New Roman;">
+							<li class="active">
+								<a href="index.php">Home</a>
+							</li>
+						</ul>
+						
+						<ul class="nav navbar-nav navbar-right" style="font-family: Times New Roman;">
+							<li>
+								<a href="login.php">
+									Login
+									<span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>
+								</a>
+							</li>
+
+							<li>
+								<?php include_once('goto-registration.php'); ?>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		
+		<div class="row">
+			<div id="carousel-id" class="carousel slide" data-ride="carousel">
+				<ol class="carousel-indicators">
+					<li data-target="#carousel-id" data-slide-to="0" class=""></li>
+					<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+					<li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+				</ol>
+				<div class="carousel-inner">
+					<div class="item">
+						<center>
+						<img src="img/1a.jpg" style="height: 100vh; width: 100%;">
+						</center>
+						<div class="container">
+							<div class="carousel-caption">
+								<h1>New Car Model</h1>
+								<p>Available Now at Low Price. Booked Now :)</p>
+								<p><a class="btn btn-lg btn-primary" href="#" role="button">Register today</a></p>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<img src="img/2a.jpg" style="height: 100vh; width: 100%;">
+						<div class="container">
+							<div class="carousel-caption">
+								<h1>#Summer</h1>
+								<p>Its More Fun in the Palompon</p>
+								<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+							</div>
+						</div>
+					</div>
+					<div class="item active">
+						<img src="img/3a.jpg" style="height: 100vh; width: 100%;">
+						<div class="container">
+							<div class="carousel-caption">
+								<h1>New Car Model.</h1>
+								<p>Envisioning a self sustaining and ecologically balanced Palompon for quality of life.</p>
+								<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+				<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+			</div>
+		</div>
+
+		<br />
+		<div class="row container-fluid">
+			<div class="col-md-4">
+				<div class="panel panel-info">
+					<div class="panel-heading">
+						<h3 class="panel-title"><center>About Us</center></h3>
+					</div>
+					<div class="panel-body">
+						The Tourism Office is located at Andhra Pradesh India. We offer Luxury cars 
+						to rent we also offer affordable lodging right in the town center.
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="panel panel-success">
+					<div class="panel-heading">
+						<h3 class="panel-title"><center>Contact Us</center></h3>
+					</div>
+					<div class="panel-body">
+						Andhra Pradesh - India
+						Municipal Tourism Officer
+						email : princepinnu55.com
+						mobile : +91 6303822413
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="panel panel-warning">
+					<div class="panel-heading">
+						<h3 class="panel-title"><center>Special Offers</center></h3>
+					</div>
+					<div class="panel-body">
+						No Offers available.
+					</div>
+				</div>
+			</div>
+		</div>
+ 		
+ 		<p>
+ 			<center>
+ 			
+ 			</center>
+ 		</p>
+		
+		<footer style="background-color: white;">
+			<center>
+				&copy; All rights reserved
+			</center>
+		</footer>
+
+ 		<script src="bootstrap/js/jquery-1.11.1.min.js"></script>
+ 		<script src="bootstrap/js/dataTables.js"></script>
+ 		<script src="bootstrap/js/dataTables2.js"></script>
+ 		<script src="bootstrap/js/bootstrap.js"></script>
+
+	</body>
+</html>
